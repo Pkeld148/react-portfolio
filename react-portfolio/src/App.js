1dc0c62c8c35908ccc8c2c99b9612e4f597ef33c
Paul Keldsen <Pkeld148@gmail.com>
2021-03-01 21:18:38 -0500

    added navbar with basic working links

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Contact from "./containers/Home/Contact/Contact";
 import Home from "./containers/Home/Home";
-
+import Header from "./components/Header/Header"
 
 
 function App() {
@@ -9,7 +9,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <Router>
-          {/* <NavbarVanilla /> */}
+          <Header />
           <Switch>
             <Route exact path="/react-portfolio" component={Home} />
             <Route exact path="/react-portfolio/contact" component={Contact} />
