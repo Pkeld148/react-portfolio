d17d2fad953d444f1a26bdc6727434f2a110a2fc
Paul Keldsen <Pkeld148@gmail.com>
2021-03-01 21:07:56 -0500

    added contact page, switched to bootstrap

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,7 +1,23 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Contact from "./containers/Home/Contact/Contact";
+import Home from "./containers/Home/Home";
+
+
+
 function App() {
   return (
     <div className="App">
-      <h1>sup world</h1>
+      <header className="App-header">
+        <Router>
+          {/* <NavbarVanilla /> */}
+          <Switch>
+            <Route exact path="/react-portfolio" component={Home} />
+            <Route exact path="/react-portfolio/contact" component={Contact} />
+
+
+          </Switch>
+        </Router>
+      </header>
     </div>
   );
 }
